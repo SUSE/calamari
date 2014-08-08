fce7048e2500646131bde1a603b3c268b75feb24
Tim Serong <tserong@suse.com>
2014-08-08 19:00:52 +1000

    Move salt files from /opt/calamari/salt to /srv/
    This moves /opt/calamari/salt/{pillar,reactor,salt} to /srv/, which is
    the directory hierarchy used in the examples listed in /etc/salt/master
    on SUSE.
    
    /opt/calamari/salt-local/ has been moved to /usr/share/calamari/salt/,
    as these files are only used internally by Calamari to configure the
    server.
    
    Note that this change will affect all distros, so shouldn't be pushed
    upstream at this time.
    
    Signed-off-by: Tim Serong <tserong@suse.com>
    (cherry picked from commit 3886bbb306cecc3a42b1df58bcabe5cb65f620d8)
    (cherry picked from commit c28c6c41027c120daae8608dc4075078bcc47ac7)

--- cthulhu/cthulhu/calamari_ctl.py
+++ cthulhu/cthulhu/calamari_ctl.py
@@ -38,7 +38,7 @@ buffer_handler.setFormatter(logging.Formatter(FORMAT))
 log.addHandler(buffer_handler)
 
 ALEMBIC_TABLE = 'alembic_version'
-POSTGRES_SLS = "/opt/calamari/salt-local/postgres.sls"
+POSTGRES_SLS = "/usr/share/calamari/salt/postgres.sls"
 
 
 @contextmanager
